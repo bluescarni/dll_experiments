d3becadd5bed6be91b7b41c3612fd421ea4e9e14
Francesco Biscani <bluescarni@gmail.com>
2017-05-14 15:29:57 +0200

    Small simplification.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <boost/dll/import.hpp> // for import_alias
+#include <functional>
 #include <iostream>
 
 #include "snopt.h"
@@ -6,12 +7,12 @@
 namespace dll = boost::dll;
 
 int main(int argc, char* argv[]) {
-    auto plugin = dll::import<void()>( // type of the function to import: function with no arguments returning void
+    std::function<void()> imported_foo = dll::import<void()>( // type of the function to import: function with no arguments returning void
         "snopt", // name of the library
         "foo", // name of the function to import
         dll::load_mode::append_decorations // makes `libmy_plugin_sum.so` or `my_plugin_sum.dll` from `my_plugin_sum`
     );
 
     // Call the imported "foo" function.
-    plugin();
+    imported_foo();
 }
